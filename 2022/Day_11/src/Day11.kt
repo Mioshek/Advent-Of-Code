9f5a9da0fde50d7ca06746d93795a79efaecc0eb
Mioshek <milosz.szczygiel@interia.pl>
2023-02-12 22:21:33 +0100

    Day11 finished in 309202μs ex1: 110264 ex2:2137620836

--- 2022/Day_11/src/Day11.kt
+++ 2022/Day_11/src/Day11.kt
@@ -1,33 +1,80 @@
 fun main(){
-    val d = Data()
-    d.initializeData()
-    val rounds = 19 // starting from 0 sum is 20 rounds
-    for (r in 0..rounds){
+    val cpath = System.getProperty("user.dir") + "/2022/inputs/Day11.txt"
+    val lines = readLines(cpath)
+    val beginTime = System.nanoTime()
+    val d1 = Data(true, lines)
+    d1.initializeData()
+    val d2 = Data(false, lines)
+    d2.initializeData()
+    var commonFactor = 1
+    for (monkey in d2.monkeys){
+        commonFactor *= monkey.test
+    }
+
+    partOne(d1)
+    partTwo(d2, commonFactor)
+    var answer = Array<Int>(2){0}
+
+    getAnswer(d1, answer)
+    println(answer[0] * answer[1])
+    answer = Array<Int>(2){0}
+    getAnswer(d2, answer)
+    val endTime = System.nanoTime()
+    println("Elapsed time in microseconds: ${(endTime-beginTime)/1000}")
+    println()
+    println(answer[0]* answer[1])
+}
+
+fun getAnswer(d: Data, answerArr: Array<Int>){
+    for (monkey in d.monkeys){
+        if (monkey.inspectedItems> answerArr[0]){
+            answerArr[1] = answerArr[0]
+            answerArr[0] = monkey.inspectedItems
+        }
+        else{
+            if (monkey.inspectedItems> answerArr[1]){
+                answerArr[1] = monkey.inspectedItems
+            }
+        }
+    }
+}
+
+fun partOne(d: Data){
+    val roundsPartOne = 19 // starting from 0 sum is 20 rounds
+    for (r in 0..roundsPartOne){
         for (monkey in d.monkeys){
             if (monkey.items.size == 0){
                 continue
             }
             else{
-                monkey.inspect()
+                monkey.inspect(1)
                 monkey.test()
             }
         }
     }
+}
 
-    for (monkey in d.monkeys){
-        println(monkey.inspectedItems)
+fun partTwo(d: Data, commonFactor: Int){
+    val roundsPartOne = 9999 // starting from 0 sum is 10_000 rounds
+    for (r in 0..roundsPartOne){
+        for (monkey in d.monkeys){
+            if (monkey.items.size == 0){
+                continue
+            }
+            else{
+                monkey.inspect(commonFactor)
+                monkey.test()
+            }
+        }
     }
 }
 
-class Data{
-    val cpath = System.getProperty("user.dir") + "/2022/inputs/Day11.txt"
-    val lines = readLines(cpath)
-    val beginTime = System.nanoTime()
+class Data(val maintableWorryLevel: Boolean,val lines: List<String>){
     val linesSize = lines.size -1
     var monkeys = mutableListOf<Monkey>()
 
     fun initializeData(){
-        var items = mutableListOf<Int>()
+        var items = mutableListOf<Long>()
         var strOperation = ""
         var test = 0
         var trueMonkey = 0
@@ -36,8 +83,8 @@ class Data{
         for (index in 0.. linesSize){
             when(index%7){
                 6 -> {currentMonkeyIndex ++
-                    monkeys.add(Monkey(items, strOperation, test, trueMonkey, falseMonkey, monkeys))
-                    items = mutableListOf<Int>()
+                    monkeys.add(Monkey(items, strOperation, test, trueMonkey, falseMonkey, monkeys, maintableWorryLevel))
+                    items = mutableListOf()
                     strOperation = ""
                     test = 0
                     trueMonkey = 0
@@ -51,7 +98,7 @@ class Data{
                 1 -> {
                     val line = lines[index].removePrefix("  Starting items: ").split(", ")
                     for (item in line){
-                        items.add(item.toInt())
+                        items.add(item.toLong())
                     }
                 }
                 0 -> continue
@@ -62,38 +109,52 @@ class Data{
 
 
 class Monkey(
-    var items: MutableList<Int>,
+    var items: MutableList<Long>,
     val strOperation: String,
     val test: Int,
     val trueMonkey: Int,
     val falseMonkey: Int,
-    val monkeys: MutableList<Monkey>
+    val monkeys: MutableList<Monkey>,
+    val maintableWorryLevel: Boolean
 ){
     val operationSign = strOperation.split(" ")[6]
     var inspectedItems = 0
 
-    fun inspect(){
+    fun inspect(commonFactor: Int){
         for (index in 0 until items.size){
             inspectedItems ++
             if (operationSign == "+" ){
-                items[index] = add(items[index])/3
+                if (maintableWorryLevel){
+                    items[index] = add(items[index])/3
+                }
+                else{
+                    items[index] = add(items[index]) % commonFactor
+//                    println(items[index])
+                }
+
             }
             else{
-                items[index] = multiply(items[index])/3
+                if (maintableWorryLevel){
+                    items[index] = multiply(items[index])/3
+                }
+                else{
+                    items[index] = multiply(items[index]) % commonFactor
+//                    println(items[index])
+                }
             }
         }
     }
 
-    private fun add(old: Int): Int {
-        return old + strOperation.split(" ").last().toInt()
+    private fun add(old: Long): Long {
+        return old + strOperation.split(" ").last().toLong()
     }
 
-    private fun multiply(old: Int): Int {
+    private fun multiply(old: Long): Long {
         val toMultiply = strOperation.split(" ").last()
         if (toMultiply == "old"){
             return old * old
         }
-        return old * toMultiply.toInt()
+        return old * toMultiply.toLong()
     }
 
     fun test(){
@@ -101,7 +162,7 @@ class Monkey(
         for (i in 0 until tmpSize){
             val item = items[0]
             items.removeAt(0)
-            if (item % test == 0){
+            if ((item % test).toInt() == 0){
                 monkeys[trueMonkey].items.add(item)
             }
             else{
